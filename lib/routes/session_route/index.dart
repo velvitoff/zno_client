ae270dde6d9930efa0513e6087cf86eee15fe588
velvitoff <velvitoff@gmail.com>
2023-05-22 23:19:50 +0300

    slight refactor

--- lib/routes/session_route/index.dart
+++ lib/routes/session_route/index.dart
@@ -1,6 +1,10 @@
 import 'package:client/dto/session_data.dart';
+import 'package:client/routes/session_route/session_display.dart';
 import 'package:client/routes/session_route/session_route_provider.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/zno_bottom_navigation_bar.dart';
+import '../../widgets/zno_top_header_text.dart';
 
 class SessionRoute extends StatelessWidget {
   final SessionData dto;
@@ -15,6 +19,17 @@ class SessionRoute extends StatelessWidget {
     return Scaffold(
       body: SessionRouteProvider(
         sessionData: dto,
+        child: Column(
+          children: [
+            ZnoTopHeaderText(text: dto.subjectName),
+            const Expanded(
+              child: Center(
+                child: SessionDisplay(),
+              ),
+            ),
+            const ZnoBottomNavigationBar(activeIndex: 0),
+          ],
+        ),
       )
     );
   }
--- lib/routes/session_route/session_route_provider.dart
+++ lib/routes/session_route/session_route_provider.dart
@@ -1,34 +1,23 @@
 import 'package:client/models/session_route_model.dart';
-import 'package:client/routes/session_route/session_display.dart';
 import 'package:flutter/material.dart';
 import 'package:client/dto/session_data.dart';
 import 'package:provider/provider.dart';
-import 'package:client/widgets/zno_bottom_navigation_bar.dart';
-import 'package:client/widgets/zno_top_header_text.dart';
 
 class SessionRouteProvider extends StatelessWidget {
   final SessionData sessionData;
+  final Widget child;
 
   const SessionRouteProvider({
     Key? key,
-    required this.sessionData
+    required this.sessionData,
+    required this.child
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => SessionRouteModel(sessionData: sessionData),
-      child: Column(
-        children: [
-          ZnoTopHeaderText(text: sessionData.subjectName),
-          const Expanded(
-            child: Center(
-              child: SessionDisplay(),
-            ),
-          ),
-          const ZnoBottomNavigationBar(activeIndex: 0),
-        ],
-      ),
+      child: child,
     );
   }
 }
