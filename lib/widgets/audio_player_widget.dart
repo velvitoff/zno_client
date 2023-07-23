af4fb39740e29f33a7c5f868e8cd146e64e81165
velvitoff <velvitoff@gmail.com>
2023-07-24 00:21:51 +0300

    audio player functionality

--- lib/widgets/audio_player_widget.dart
+++ lib/widgets/audio_player_widget.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:client/services/interfaces/storage_service_interface.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../locator.dart';
 
@@ -20,18 +23,78 @@ class AudioPlayerWidget extends StatefulWidget {
 }
 
 class _AudioPlayerState extends State<AudioPlayerWidget> {
+  late final Source source;
+  late final List<StreamSubscription> subscriptions;
   final player = AudioPlayer();
+
+  bool isPlaying = false;
+  Duration duration = Duration.zero;
+  Duration position = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    source = DeviceFileSource(locator
+        .get<StorageServiceInterface>()
+        .getImagePath(
+            widget.folderName, widget.folderInnerName, widget.fileName));
+
+    player.setSource(source);
+
+    subscriptions = [];
+    subscriptions.addAll([
+      player.onPlayerStateChanged.listen((state) => setState(() {
+            isPlaying = state == PlayerState.playing;
+          })),
+      player.onDurationChanged.listen((newDuration) => setState(() {
+            duration = newDuration;
+          })),
+      player.onPositionChanged.listen((newPosition) => setState(() {
+            position = newPosition;
+          }))
+    ]);
+  }
+
+  @override
+  void dispose() {
+    for (var sub in subscriptions) {
+      sub.cancel();
+    }
+    player.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        player.play(DeviceFileSource(locator
-            .get<StorageServiceInterface>()
-            .getImagePath(
-                widget.folderName, widget.folderInnerName, widget.fileName)));
-      },
-      child: Text("Audio"),
+    return Container(
+      margin: EdgeInsets.only(bottom: 10.h),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          GestureDetector(
+            onTap: () {
+              if (isPlaying) {
+                player.pause();
+              } else {
+                player.play(source);
+              }
+              setState(() {
+                isPlaying = !isPlaying;
+              });
+            },
+            child: Icon(isPlaying ? Icons.pause_circle : Icons.play_circle,
+                size: 50.r),
+          ),
+          Slider(
+            min: 0,
+            max: duration.inSeconds.toDouble(),
+            value: position.inSeconds.toDouble(),
+            onChanged: (value) => player.seek(Duration(seconds: value.toInt())),
+          ),
+          Text(
+              '${position.inMinutes}:${(position.inSeconds % 60).toString().padLeft(2, '0')} / ${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}')
+        ],
+      ),
     );
   }
 }
