f7df848a9014abfa327626f36b123e7c54c4a263
velvitoff <velvitoff@gmail.com>
2023-09-19 17:02:45 +0300

    payment progress

--- lib/routes/premium_route/button_google_pay.dart
+++ lib/routes/premium_route/button_google_pay.dart
@@ -1,43 +1,6 @@
+import 'package:client/payment_config.dart';
 import 'package:flutter/material.dart';
 import 'package:pay/pay.dart';
-
-const String defaultGooglePayConfigString = '''{
-  "provider": "google_pay",
-  "data": {
-    "environment": "TEST",
-    "apiVersion": 2,
-    "apiVersionMinor": 0,
-    "allowedPaymentMethods": [
-      {
-        "type": "CARD",
-        "tokenizationSpecification": {
-          "type": "PAYMENT_GATEWAY",
-          "parameters": {
-            "gateway": "example",
-            "gatewayMerchantId": "gatewayMerchantId"
-          }
-        },
-        "parameters": {
-          "allowedCardNetworks": ["VISA", "MASTERCARD"],
-          "allowedAuthMethods": ["PAN_ONLY", "CRYPTOGRAM_3DS"],
-          "billingAddressRequired": true,
-          "billingAddressParameters": {
-            "format": "FULL",
-            "phoneNumberRequired": true
-          }
-        }
-      }
-    ],
-    "merchantInfo": {
-      "merchantId": "01234567890123456789",
-      "merchantName": "Example Merchant Name"
-    },
-    "transactionInfo": {
-      "countryCode": "US",
-      "currencyCode": "USD"
-    }
-  }
-}''';
 
 class ButtonGooglePay extends StatelessWidget {
   const ButtonGooglePay({super.key});
@@ -57,8 +20,8 @@ class ButtonGooglePay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GooglePayButton(
-        paymentConfiguration:
-            PaymentConfiguration.fromJsonString(defaultGooglePayConfigString),
+        paymentConfiguration: PaymentConfiguration.fromJsonString(
+            PaymentConfig.defaultGooglePayString),
         onPaymentResult: onGooglePayResult,
         paymentItems: _paymentItems);
   }
